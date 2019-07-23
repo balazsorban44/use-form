2071b4a6ce8f263bee98fc9516ea17da85a97c95
Balázs Orbán <info@balazsorban.com>
2019-07-23 15:26:06 +0200

    feat(FormContext): add async initialState
    By defining a useEffect, we can
    set the initialState even after the usereducer was set up.

--- src/FormContext.js
+++ src/FormContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect, useRef } from 'react'
 
 const FormContext = createContext()
 
@@ -8,10 +8,24 @@ function reducer(state, { type, payload }) {
 
   if (type in state) return { ...state, [type]: { ...state[type], ...payload } }
 
+  if (type === 'SET_DEFAULT') return { ...payload }
 }
 
 function FormProvider({ children, initialState }) {
   const [forms, dispatch] = useReducer(reducer, initialState)
+
+
+  /**
+   * We override initialState if the prop has changed.
+   * Useful for asynchronously fetched initialStates.
+   */
+  const initialStateRef = useRef(initialState)
+  useEffect(() => {
+    if (JSON.stringify(initialState) !== JSON.stringify(initialStateRef.current)) {
+      dispatch({ type: 'SET_DEFAULT', payload: initialState })
+    }
+  }, [initialState])
+
   return (
     <FormContext.Provider value={{ forms, dispatch }}>
       {children}
